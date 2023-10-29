ccecaa53e1b1a56d39b8ae391328ef04df909736
Imrane ALI LAFKIH <imr07ane@gmail.com>
2023-10-29 13:31:54 -0700

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-"""Flask server
-"""
+'''
+Flask app
+'''
 
-
-from flask import Flask, jsonify
-from models import storage
 from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
+from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+
+# enable CORS and allow for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def teardown_engine(exception):
     storage.close()
 
-
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
-
-
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
