4b87a4a4df47e5c291482dfae934363a52abd810
Imrane ALI LAFKIH <imr07ane@gmail.com>
2023-10-29 14:18:47 -0700

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-'''
-Flask app
-'''
+"""
+Flask app 
+"""
 
-from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
-from models import storage
+from os import getenv
+
 from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
 
-# enable CORS and allow for origins:
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_engine(exception):
+def teardown(exception):
+    """
+    teardown
+    """
     storage.close()
 
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-'''
+"""
 API status
-'''
-
+"""
 
 from flask import jsonify
 from api.v1.views import app_views
+
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def api_status():
-    '''
-    Returns a JSON response for RESTful API health.
-    '''
-    response = {'status': 'OK'}
-    return jsonify(response)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    """
+    data = {
+        "status": "OK"
+    }
+
+    result = jsonify(data)
+    result.status_code = 200
+
+    return result
