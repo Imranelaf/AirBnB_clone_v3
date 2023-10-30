6906e70627c78b9bb548e53b0b20c75c0ce34477
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-10-30 15:00:12 -0700

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,34 @@
 #!/usr/bin/python3
-"""
-API satuts
-"""
+'''
+API status
+'''
+
 
 from flask import jsonify
 from api.v1.views import app_views
-
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
-def status():
-    """
-    status route
-    :return: response with json
-    """
-    data = {
-        "status": "OK"
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    '''
+    Returns a JSON response for RESTful API health.
+    '''
+    response = {'status': 'OK'}
+    return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    '''
+    Retrieves the number of each objects by type.
+    '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
     }
-
-    result = jsonify(data)
-    result.status_code = 200
-
-    return result
-
-
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
-def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    result = jsonify(data)
-    result.status_code = 200
-
-    return result
+    return jsonify(stats)
